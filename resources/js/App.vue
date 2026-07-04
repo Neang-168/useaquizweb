6f4a94ef2aca74f2de6ae82a986f03b9b65e5513
Neang-168 <chhomsreyneang0@gmail.com>
2026-07-04 15:32:55 +0700

    Test branch

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="min-h-screen flex flex-col justify-center items-center gap-6 bg-white">
         <h1 class="text-4xl font-bold text-blue-600">
-            Laravel 12 + Vue 3 + PrimeVue
+            Laravel 12 + Vue 3 + PrimeVue 
         </h1>
 
-        <Button label="PrimeVue Works!" icon="pi pi-check" severity="warning" />
+        <Button label="Development Branch!" icon="pi pi-check" severity="warning" />
 
     </div>
 </template>

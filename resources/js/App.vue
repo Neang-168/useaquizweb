59f31a52210879f235c368da103c6578180f7760
Neang-168 <chhomsreyneang0@gmail.com>
2026-07-04 15:26:18 +0700

    First Commit

---
+++ resources/js/App.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="min-h-screen flex flex-col justify-center items-center gap-6 bg-white">
+        <h1 class="text-4xl font-bold text-blue-600">
+            Laravel 12 + Vue 3 + PrimeVue
+        </h1>
+
+        <Button label="PrimeVue Works!" icon="pi pi-check" severity="warning" />
+
+    </div>
+</template>
+<script setup>
+</script>
